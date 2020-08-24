dd145ec9d8a3d7821b98193afbfbed5ece23ae60
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-08-24 15:35:43 -0300

    test project based off of amethyst 2D template

---
+++ src/transparency_system.rs
@@ -0,0 +1,35 @@
+use amethyst::{
+    core::timing::Time,
+    ecs::{Join, Read, System, WriteStorage},
+    renderer::resources::Tint,
+};
+
+pub struct TransparencySystem;
+
+impl<'s> System<'s> for TransparencySystem {
+    type SystemData = (WriteStorage<'s, Tint>, Read<'s, Time>);
+
+    fn run(&mut self, (mut tints, time): Self::SystemData) {
+        let mut count = 0;
+        for tint in (&mut tints).join() {
+            let time: f32 = time.absolute_time().as_secs_f32() as f32 * 4.0;
+
+            let value = (0.5 + time.sin() * 0.51).max(0.0).min(1.0);
+
+            if count < 3 {
+                tint.0.red = value;
+                tint.0.green = value;
+                tint.0.blue = value;
+            } else if count < 6 {
+                tint.0.red = value;
+                tint.0.green = value;
+                tint.0.blue = value;
+                tint.0.alpha = value;
+            } else {
+                tint.0.alpha = value;
+            }
+
+            count += 1;
+        }
+    }
+}
